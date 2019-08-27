cc7941827292a391da7984539dbb8162e0d1c790
adriana cortes <adrianm-cortesv@unilibre.edu.co>
2019-08-26 20:23:48 -0500

    primero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'app';
+  var1 = 10;
+  var2 = 20;
+  deshabilitado = true;
+  nombre: string;
+  paises:any =[
+  {activo: true, nombre:"Colombia"},
+  {activo: true, nombre:"Brasil"},
+  {activo: false, nombre:"Argentina"},
+  {activo: true, nombre:"Polonia"},
+  {activo: false, nombre:"Alemania"},
+  {activo: true, nombre:"Francia"}
+  ]
+  lat:number = 51.778418;
+  lng:number = 7.809007;
+  constructor(){
+  setTimeout(() =>{
+  this.deshabilitado = false;
+  }, 3000)
+
+
+  }
+  ejecutar(){
+  alert("Ejecutando...")
+  }
+
+
+num1:number;
+  num2:number;
+  rta:number;
+  operacion(num1:number,num2:number,opera:string): number
+  {
+    switch(opera)
+    {
+      case "suma":
+        return this.rta = this.num1 + this.num2;
+      case "resta":
+        return this.rta = this.num1 - this.num2;
+      case "multiplicacion":
+        return this.rta = this.num1 * this.num2;
+      case "dividir":
+        if(this.num2 == 0)
+        {
+        alert("No se puede dividr por cero");
+        return null;
+        }
+        else
+        {
+        return this.rta = this.num1 / this.num2;
+        }
+      case "modulo":
+        if(this.num2 == 0)
+        return null;
+        else
+        return this.rta = this.num1 % this.num2;
+      case "limpiar":
+        return null;
+    }
+  }
+  reset()
+  {
+    this.num1 = null;
+    this.num2 = null;
+    this.rta = null;
+  }
+
+
+val1:number;
+val2:number;
+sum:number;
+res:number;
+mult:number;
+div:number;
+mod:number;
+
+Operacion(){
+this.sum=this.val1+this.val2;
+this.res=this.val1-this.val2;
+this.mult=this.val1*this.val2;
+this.div=this.val1/this.val2;
+this.mod= this.val1%this.val2;
+  
+  }
+clear()
+  {
+    this.val1 = null;
+    this.val2 = null;
+    this.sum = null;
+    this.res = null;
+    this.mult = null;
+    this.div = null;
+    this.mod = null;
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {FormsModule} from '@angular/forms';
+import {AgmCoreModule} from '@agm/core'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+    apiKey: 'AIzaSyBSExHjCF5YE7ZBNiGPDeV-OD40NMrsXIE'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
